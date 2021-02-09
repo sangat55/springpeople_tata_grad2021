10e291973edc925c576c90d9439e46c23be59188
sangat55 <78354478+sangat55@users.noreply.github.com>
2021-02-09 11:49:27 +0545

    Update CDTest.java

--- Day2_SangatBaral_CDList/Assignment2/src/test/java/com/tata/assignment/tests/CDTest.java
+++ Day2_SangatBaral_CDList/Assignment2/src/test/java/com/tata/assignment/tests/CDTest.java
@@ -37,4 +37,18 @@ public class CDTest {
     {
         assertTrue(singerId>0);
     }
+    @Test
+    @DisplayName("Test whether generated CDs are sorted")
+    public void testCdTitleUnique()
+    {
+        CDList cdL = new CDListImpl();
+        List<CD> cdList = cdL.getCDList();
+        List<CD> cdList1,cdList2;
+        cdList1 = new ArrayList<>(cdList);
+        cdList1.stream().sorted((o1,o2)->o1.getSinger().compareTo(o2.getSinger()));
+        cdList2 = new ArrayList<>(cdList);
+        CDApp cdApp = new CDApp();
+        cdList2 = Collections.sort(cdList,new CDSorter());
+        assertEquals(cdList1,cdList2);
+    }
 }
