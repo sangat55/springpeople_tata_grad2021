52bd0f88dd464902c04fa7e29506417d036af5e5
sangat55 <78354478+sangat55@users.noreply.github.com>
2021-02-21 23:02:31 +0545

    Delete ShoppersDen directory

--- ShoppersDen/src/main/java/com/tata/shoppersden/dao/CategoryDao.java
+++
@@ -1,9 +0,0 @@
-package com.tata.shoppersden.dao;
-
-import com.tata.shoppersden.models.Category;
-
-import java.sql.SQLException;
-
-public interface CategoryDao {
-    public long getRandomCategoryId() throws SQLException;
-}
